70875827875fe045510411d59032a582f226ef67
Paro <cooparo@proton.me>
2024-03-16 21:26:18 +0100

    Better indent

--- flake.nix
+++ flake.nix
@@ -3,28 +3,28 @@
     
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-23.11";
-	home-manager.url = "github:nix-community/home-manager/release-23.11";
-	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	    home-manager.url = "github:nix-community/home-manager/release-23.11";
+	    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
     outputs = { self, nixpkgs, home-manager, ... }:
         let
             lib = nixpkgs.lib;
-	    system = "x86_64-linux";
-	    pkgs = nixpkgs.legacyPackages.${system};
+	        system = "x86_64-linux";
+	        pkgs = nixpkgs.legacyPackages.${system};
         in {
             nixosConfigurations = {
                 nixos = lib.nixosSystem {
-		    inherit system;
-                    modules = [ ./configuration.nix ];
-                };
+		        inherit system;
+                modules = [ ./configuration.nix ];
             };
+        };
 
 	    homeConfigurations = {
                 paro = home-manager.lib.homeManagerConfiguration {
 		    inherit pkgs;
-                    modules = [ ./home.nix ];
-                };
+                modules = [ ./home.nix ];
             };
         };
+    };
 }
