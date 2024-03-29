3b2eeb92fae764833f501875ed28130da1a37d85
cooparo <cooparo@proton.me>
2024-03-29 14:57:53 +0100

    Added user and system stylix (does't work)

---
+++ user/style/stylix.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs, stylix, ... }: 
+{
+    imports = [ stylix.homeManagerModules.stylix ];
+
+    stylix = {
+        autoEnable = false;
+
+        image = pkgs.fetchurl {
+            url = "https://c4.wallpaperflare.com/wallpaper/446/361/284/nord-nord-pack-hd-wallpaper-preview.jpg";
+            sha256 = "f6cd0314f8d0a177ab3348050a944153dc7d11b0e75c48e1b058603429475066"; 
+        };
+        
+        polarity = "dark";
+        base16Scheme = ./themes/${userSettings.theme}/${userSettings.theme}.nix;
+    
+        targets.console.enable = true;
+    };
+    
+}
