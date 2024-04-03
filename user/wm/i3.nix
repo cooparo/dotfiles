b54d0e8e10db3686672170acfd1eb650af381749
cooparo <cooparo@proton.me>
2024-04-03 23:02:32 +0200

    i3 window.titlebar set to false

--- user/wm/i3.nix
+++ user/wm/i3.nix
@@ -2,17 +2,25 @@
 {
     xsession.windowManager.i3 = {
         enable = true;
+
         config = {
             modifier = "Mod4"; # Windows button
             terminal = userSettings.term;
             focus.followMouse = false;
 
+            window.titlebar = false;
+
             gaps.inner = 5;
             gaps.outer = 2;
 
+            # Start up services
             startup = [
+                # Wallpaper
                 { command = "feh --bg-fill ${userSettings.dotfilesDir}/wallpaper.jpg"; always = true; notification = false; }
+                
+                # Reload
                 { command = "xrandr --output Virtual-1 --mode 1920x1080"; always=true; notification = false; }
+
             ];
         };
     };
