64913297b53796c85c114a741d62ae7fde1c23d8
cooparo <cooparo@proton.me>
2024-04-07 12:19:32 +0200

    Fixed warning useOSProber set to false

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -22,7 +22,7 @@
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.useOSProber = false;
 
   networking.hostName = systemSettings.hostname; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
