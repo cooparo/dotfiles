bb34f40153fa38b216cd704ba14f37514236bd35
cooparo <cooparo@proton.me>
2024-04-08 13:46:56 +0200

    Removed i3 extraPackages

--- system/desktopEnv/i3.nix
+++ system/desktopEnv/i3.nix
@@ -1,13 +1,4 @@
 { pkgs, ... }:
 {
-    services.xserver = {
-        windowManager.i3 = {
-            enable = true;
-
-            # TODO: remove extraPackages
-            extraPackages = with pkgs; [
-                i3status # gives you the default i3 status bar
-            ];
-        };
-    };
+    services.xserver.windowManager.i3.enable = true;
 }
