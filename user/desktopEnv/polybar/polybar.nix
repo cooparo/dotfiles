301c5df7d32a07d2a0493e0fd501cf99b0c48c5e
cooparo <cooparo@proton.me>
2024-04-11 11:32:07 +0200

    Added theming to polybar

--- user/desktopEnv/polybar/polybar.nix
+++ user/desktopEnv/polybar/polybar.nix
@@ -1,11 +1,14 @@
-{ ... }:
+{ userSettings, ... }:
 {
 
     services.polybar = {
         enable = true;
 
         script = "polybar top-bar &"; # Don't remove this or will throw an error
+        extraConfig = 
+        ''
+        include-file = ${userSettings.dotfilesDir}/user/desktopEnv/polybar/themes/${userSettings.theme}.ini
+        include-file = ${userSettings.dotfilesDir}/user/desktopEnv/polybar/config.ini
+        '';
     };
-
-    home.file.".config/polybar/config.ini".source = ./config.ini;
 }
