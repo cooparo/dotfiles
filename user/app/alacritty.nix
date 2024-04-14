fe460a7425fd3a71603bdc9c6d2c77c4d3337183
cooparo <cooparo@proton.me>
2024-04-14 17:58:15 +0200

    Added nix-colors to alacritty

--- user/app/alacritty.nix
+++ user/app/alacritty.nix
@@ -1,6 +1,5 @@
-{lib, ...}: 
+{ config, ...}: 
 {
-    # TODO: make it modular (use nix color or stylix)
     programs.alacritty = {
         enable = true;
         settings = {
@@ -9,27 +8,19 @@
                 padding = {x=5; y=5;};
                 dynamic_padding = true;
             };
-            colors = {
-                primary.background = "#282828";
-                primary.foreground = "#ebdbb2";
-                cursor.text = "#689d6a";
-                cursor.cursor = "#8ec07c";
-                normal.black = "#1d2021";
-                normal.red = "#cc241d";
-                normal.green = "#98971a";
-                normal.yellow = "#d79921";
-                normal.blue = "#458588";
-                normal.magenta = "#d3869b";
-                normal.cyan = "#83a598";
-                normal.white = "#fbf1c7";
-                bright.black = "#3c3836";
-                bright.red = "#fb4934";
-                bright.green = "#b8bb26";
-                bright.yellow = "#fabd2f";
-                bright.blue = "#458588";
-                bright.magenta = "#d3869b";
-                bright.cyan = "#83a598";
-                bright.white = "#fbf1c7";
+            colors = with config.colorScheme.palette; {
+                primary.background = "#${base00}";
+                primary.foreground = "#${base06}";
+                cursor.text = "#${base0F}";
+                cursor.cursor = "#${base0C}";
+                normal.black = "#${base00}";
+                normal.red = "#${base08}";
+                normal.green = "#${base0B}";
+                normal.yellow = "#${base0A}";
+                normal.blue = "#${base0D}";
+                normal.magenta = "#${base0E}";
+                normal.cyan = "#${base0C}";
+                normal.white = "#${base07}";
             };  
         };
     };
