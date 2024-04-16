a74958012592cb82c270c2898ef78421dc0ad41b
cooparo <cooparo@proton.me>
2024-04-16 13:26:03 +0200

    Moved ~/wallpaper.jpg to ~/.wallpaper.jpg

--- user/desktopEnv/i3.nix
+++ user/desktopEnv/i3.nix
@@ -31,7 +31,7 @@ in {
             # Start up services
             startup = [
                 # Set wallpaper
-                { command = "feh --bg-fill ~/wallpaper.jpg"; always = true; notification = false; }
+                { command = "feh --bg-fill ~/.wallpaper.jpg"; always = true; notification = false; }
                 # Reload display definition
                 { command = "xrandr --output Virtual-1 --mode ${systemSettings.resolution}"; always=true; notification = false; }
                 # Picom
