8371f5656bbabb7392b35f416ab1905383477a12
cooparo <cooparo@proton.me>
2024-04-16 19:04:36 +0200

    Added gtk theming

--- user/desktopEnv/gtk.nix
+++ user/desktopEnv/gtk.nix
@@ -1,6 +1,19 @@
-{ ... }:
+{ pkgs, userSettings, ... }:
 {
-    gtk = {
+    home.packages = with pkgs; [
+        nordic
+        gruvbox-dark-gtk
+    ];
+
+    gtk = with pkgs; {
         enable = true;
+
+        # theme = {
+        #     package = if (userSettings.theme == "nord") then nordic else gruvbox-dark-gtk;
+        #     name = "Nord"; 
+        # };
+        iconTheme.name = "Qogir";
+        theme.name = "Nordic";
+        
     };
 }
