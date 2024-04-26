7fda6f36392ccfcc4f0f3347bc8524da6ad55ccf
cooparo <cooparo@proton.me>
2024-04-26 13:59:27 +0200

    Added some nixvim lsp and lualine theming

--- user/app/nixvim/plugins.nix
+++ user/app/nixvim/plugins.nix
@@ -1,14 +1,28 @@
-{ ... }:
+{ userSettings, ... }:
 {
   programs.nixvim.plugins = {
+
     lualine.enable = true;
+    lualine.theme = userSettings.theme;
+
+    # telescope.enable = true;
     
     lsp = {
       enable = true;
 
       servers = {
+
         lua-ls.enable = true;
         lua-ls.settings.telemetry.enable = false;
+
+        # Nix lang
+        nixd.enable = true;
+        nixd.autostart = true;
+
+        # C/C++
+        clangd.enable = true;
+        clangd.autostart = true;
+
       };
     };
   };
