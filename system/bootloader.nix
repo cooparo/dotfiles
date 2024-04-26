5e3f0706d217278226d3be18941905f3d81b72b5
cooparo <cooparo@proton.me>
2024-04-26 13:58:24 +0200

    Switched bootloader to use GRUB

---
+++ system/bootloader.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  # Uses GRUB bootloader
+  # from https://github.com/mcdonc/.nixconfig/blob/master/videos/noboot/script.rst
+
+  boot.loader.grub.enable = true;
+  # This causes NixOS to only change any existing GRUB menu files,
+  # but not to try to actually install the GRUB boot loader to any partition
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
+}
