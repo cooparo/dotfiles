177127320ad3c9b8d801fd822b61ea81693052b8
cooparo <cooparo@proton.me>
2024-05-19 14:55:57 +0200

    added alias for anon

--- host/anon/home.nix
+++ host/anon/home.nix
@@ -7,6 +7,7 @@
 
     ./user/i3.nix # Startup command
     ./user/alacritty.nix # Fixes fontSize
+    ./user/zsh.nix # Aliases
   ];
 
   home.packages = with pkgs; [
---
+++ host/anon/user/zsh.nix
@@ -0,0 +1,6 @@
+{ ... }:
+{
+  programs.zsh.shellAliases = {
+    rdesktop="ssh paro@100.121.16.98 -i ~/.ssh/desktop-ssh";
+  };
+}
