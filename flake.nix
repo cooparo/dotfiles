3bcdf2446ab72924785f7195d66d3f6bbf2f8486
cooparo <cooparo@proton.me>
2024-05-19 11:28:19 +0200

    removed profiles dir and used hostname
    Switched to profiles based flakes to hostname based, doesn't make big
    difference but profiles was a useless redundancy

--- flake.nix
+++ flake.nix
@@ -1,85 +1,84 @@
 {
-    description = "Paro's flake";
-    
-    outputs = { nixpkgs, nixpkgs-stable, home-manager, ... } @ inputs:
-        let
-            # --- SYSTEM SETTINGS --- #
-            systemSettings = {
-                system = "x86_64-linux";
-                hostname = "nixos"; # hostname
-                profile = "desktop"; # select a profile defined from my profiles directory
-                timezone = "Europe/Rome"; # select timezone
-                locale = "en_US.UTF-8"; # select locale
-                resolution = "1920x1080";
-            };
+  description = "Paro's flake";
 
-            # --- USER SETTINGS --- #
-            userSettings = {
-                username = "paro";
-                email = "cooparo@proton.me";
-                dotfilesDir = "/home/paro/dotfiles";
-                
-                term = "alacritty";
-                editor = "nvim";
-                browser = "brave";
-                shell = "zsh";
-
-                eth-interface = "eno1";
-                wifi-interface = "";
-
-                nerdfonts = [ "FiraCode" "Iosevka" ];
-                kbLayout = "us, it";
-                fontSize = 11;
-                theme = "nord"; 
-                # theme = "gruvbox-dark-medium"; 
-            };
-
-            # Config lib
-            lib = nixpkgs.lib;
-
-            # Nixpkgs
-	        pkgs = nixpkgs.legacyPackages.${systemSettings.system};
-            pkgs-stable = nixpkgs-stable.legacyPackages.${systemSettings.system};
-
-        in {
-            nixosConfigurations.${systemSettings.hostname} = lib.nixosSystem {
-                system = systemSettings.system;
-                modules = [ ./profiles/${systemSettings.profile}/configuration.nix ];
-
-                specialArgs = {
-                    inherit userSettings;
-                    inherit systemSettings;
-                    inherit pkgs-stable;
-                    inherit (inputs) nixos-hardware;
-                };
-            }; # END nixosConfigurations
-
-    	    homeConfigurations.${userSettings.username} = home-manager.lib.homeManagerConfiguration {
-                inherit pkgs;
-                modules = [ ./profiles/${systemSettings.profile}/home.nix ];
-
-                extraSpecialArgs = {
-                    inherit userSettings;
-                    inherit systemSettings;
-                    inherit pkgs-stable;
-                    inherit (inputs) nix-colors;
-                    inherit (inputs) nixvim;
-                };
-            }; # END homeConfigurations
-        };
-
-    inputs = {
-        nixpkgs-stable.url = "nixpkgs/nixos-23.11";
-        nixpkgs.url = "nixpkgs/nixos-unstable";
-
-	    home-manager.url = "github:nix-community/home-manager/master";
-	    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-        nix-colors.url = "github:misterio77/nix-colors";
-
-        nixvim.url = "github:nix-community/nixvim";
-        nixvim.inputs.nixpkgs.follows = "nixpkgs";
-
-        nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  outputs = { nixpkgs, nixpkgs-stable, home-manager, ... } @ inputs:
+    let
+# --- SYSTEM SETTINGS --- #
+    systemSettings = {
+      system = "x86_64-linux";
+      host = "desktop"; # hostname
+	timezone = "Europe/Rome"; # select timezone
+	locale = "en_US.UTF-8"; # select locale
+	resolution = "1920x1080";
     };
+
+# --- USER SETTINGS --- #
+  userSettings = {
+    username = "paro";
+    email = "cooparo@proton.me";
+    dotfilesDir = "/home/paro/dotfiles";
+
+    term = "alacritty";
+    editor = "nvim";
+    browser = "brave";
+    shell = "zsh";
+
+    eth-interface = "eno1";
+    wifi-interface = "";
+
+    nerdfonts = [ "FiraCode" "Iosevka" ];
+    kbLayout = "us, it";
+    fontSize = 11;
+    theme = "nord"; 
+    # theme = "gruvbox-dark-medium"; 
+  };
+
+  # Config lib
+  lib = nixpkgs.lib;
+
+  # Nixpkgs
+  pkgs = nixpkgs.legacyPackages.${systemSettings.system};
+  pkgs-stable = nixpkgs-stable.legacyPackages.${systemSettings.system};
+
+  in {
+    nixosConfigurations.${systemSettings.host} = lib.nixosSystem {
+      system = systemSettings.system;
+      modules = [ ./host/${systemSettings.host}/configuration.nix ];
+
+      specialArgs = {
+	inherit userSettings;
+	inherit systemSettings;
+	inherit pkgs-stable;
+	inherit (inputs) nixos-hardware;
+      };
+    }; # END nixosConfigurations
+
+    homeConfigurations.${userSettings.username} = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./host/${systemSettings.host}/home.nix ];
+
+      extraSpecialArgs = {
+	inherit userSettings;
+	inherit systemSettings;
+	inherit pkgs-stable;
+	inherit (inputs) nix-colors;
+	inherit (inputs) nixvim;
+      };
+    }; # END homeConfigurations
+  };
+
+  inputs = {
+    nixpkgs-stable.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-colors.url = "github:misterio77/nix-colors";
+
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
 }
--- system/networking.nix
+++ system/networking.nix
@@ -1,6 +1,6 @@
 { lib, systemSettings, ... }:
 {
-  networking.hostName = systemSettings.hostname; # Define your hostname.
+  networking.hostName = systemSettings.host; # Define your hostname.
 
   # Enables wireless support via wpa_supplicant.
   networking.wireless.enable = lib.mkDefault false; 
