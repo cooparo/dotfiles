e4521ceafff41ad68708317a9ea59b020b9c45ba
cooparo <cooparo@proton.me>
2024-08-31 15:30:11 +0200

    Dunst cooler notification pt2

--- user/desktopEnv/dunst.nix
+++ user/desktopEnv/dunst.nix
@@ -10,10 +10,21 @@
 	offset = "20x50";
 	transparency = 5;
 	notification_limit = 5;
-	frame_color = "#${base04}";
-	frame_width = 0;
+	frame_color = "#${base0E}";
+	background = "#${base02}";
+	foreground = "#${base07}";
+	frame_width = 1;
 	corner_radius = 5;
 	gap_size = 3;
+      };
+
+      urgency_low = with config.colorScheme.palette; {
+	frame_color = "#${base02}";
+      };
+
+      urgency_critical = with config.colorScheme.palette; {
+	frame_color = "#${base08}";
+	background = "#${base08}";
       };
     };
   };
