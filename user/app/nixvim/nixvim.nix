9e8568def06439d3d5b3a9eb77807dfd71e453eb
cooparo <cooparo@proton.me>
2024-09-22 20:10:08 +0200

    Fixed formatting

--- user/app/nixvim/nixvim.nix
+++ user/app/nixvim/nixvim.nix
@@ -1,54 +1,54 @@
 { pkgs, nixvim, userSettings, lib,  ... }:
 {
   imports = [ 
-		nixvim.homeManagerModules.nixvim
-		./keymaps.nix
-		./plugins/default.nix
-		./ui/default.nix
-	];
+    nixvim.homeManagerModules.nixvim
+    ./keymaps.nix
+    ./plugins/default.nix
+    ./ui/default.nix
+  ];
 
-	home.packages = with pkgs; [
-		xclip
-		ripgrep
-		lazygit
-	];
+  home.packages = with pkgs; [
+    xclip
+      ripgrep
+      lazygit
+  ];
 
   programs.nixvim = {
-		enable = true;
-		defaultEditor = true;
+    enable = true;
+    defaultEditor = true;
 
-		colorschemes.base16.enable = true; 
-		colorschemes.base16.colorscheme = lib.mkDefault userSettings.theme; 
+    colorschemes.base16.enable = true; 
+    colorschemes.base16.colorscheme = lib.mkDefault userSettings.theme; 
 
-		globals.mapleader = " ";
+    globals.mapleader = " ";
 
-		clipboard.register = "unnamedplus";
-		clipboard.providers.xclip.enable = true;
-		
-		opts = {
-			number = true;
-			relativenumber = true;
+    clipboard.register = "unnamedplus";
+    clipboard.providers.xclip.enable = true;
 
-			shiftwidth = 2;
-			softtabstop = 2;
-			showtabline = 2;
-			smartindent = true; # enables autoindenting
-			wrap = false;
-			breakindent = true;
-			backup = false;
-			swapfile = false;
-			hlsearch = false;
-			incsearch = true;
-			termguicolors = true;
-			scrolloff = 16;
+    opts = {
+      number = true;
+      relativenumber = true;
 
-			updatetime = 50; # faster completion (default: 4000ms)
-			completeopt = ["menuone" "noselect" "noinsert"]; # mostly just for cmp
-			undofile = true;
-			cursorline = true; # highlights the line where the cursor is located
+      shiftwidth = 2;
+      softtabstop = 2;
+      showtabline = 2;
+      smartindent = true; # enables autoindenting
+	wrap = false;
+      breakindent = true;
+      backup = false;
+      swapfile = false;
+      hlsearch = false;
+      incsearch = true;
+      termguicolors = true;
+      scrolloff = 16;
 
-			encoding = "utf-8";
-			fileencoding = "utf-8";
-		};
+      updatetime = 50; # faster completion (default: 4000ms)
+	completeopt = ["menuone" "noselect" "noinsert"]; # mostly just for cmp
+	undofile = true;
+      cursorline = true; # highlights the line where the cursor is located
+
+	encoding = "utf-8";
+      fileencoding = "utf-8";
+    };
   };
 }
