fc8e78c4393de4da204cd66743852ea6891d8dff
cooparo <cooparo@proton.me>
2024-10-24 16:52:05 -0300

    Added nix formatter and none-ls

--- user/app/nixvim/plugins/default.nix
+++ user/app/nixvim/plugins/default.nix
@@ -2,25 +2,27 @@
 {
   imports = [
     ./cmp.nix
-      ./harpoon.nix 
-      ./lsp.nix
-      ./oil.nix
+    ./harpoon.nix
+    ./lsp.nix
+    ./oil.nix
+    ./none-ls.nix
   ];
 
   programs.nixvim.plugins = {
     web-devicons.enable = true;
 
-    lualine = lib.mkForce {		
+    lualine = lib.mkForce {
       enable = true;
-      settings.options.theme = if(userSettings.theme == "nord") then userSettings.theme else "gruvbox_dark";
+      settings.options.theme =
+        if (userSettings.theme == "nord") then userSettings.theme else "gruvbox_dark";
     };
     telescope.enable = true;
-    
+
     nvim-colorizer.enable = true;
 
     none-ls.enable = true;
     none-ls.enableLspFormat = true;
-    
+
     lsp-format.enable = true;
     gitsigns.enable = true;
     nvim-autopairs.enable = true;
@@ -30,7 +32,7 @@
 
     bufferline.enable = true;
     bufferline.settings.options.always_show_bufferline = false;
-    
+
     luasnip.enable = true;
 
     neo-tree = {
@@ -43,18 +45,27 @@
 
     treesitter = {
       enable = true;
-      settings.ensure_installed = [ "c" "lua" "nix" "javascript" "typescript" "python" "rust" "bash"];
+      settings.ensure_installed = [
+        "c"
+        "lua"
+        "nix"
+        "javascript"
+        "typescript"
+        "python"
+        "rust"
+        "bash"
+      ];
     };
 
     toggleterm = {
       enable = true;
       settings = {
-	direction = "float";
-	float_opts = {
-	  border = "curved";
-	  height = 30;
-	  width = 130;
-	};
+        direction = "float";
+        float_opts = {
+          border = "curved";
+          height = 30;
+          width = 130;
+        };
       };
     };
   };
