9bf7c565b0e7f3a7d7dc1ef0257cde0f2302a7b9
cooparo <github.ambitious226@slmails.com>
2024-12-19 00:55:43 +0100

    fixed wrong attributes' location

--- user/app/zsh/zsh.nix
+++ user/app/zsh/zsh.nix
@@ -8,36 +8,38 @@
     zsh-powerlevel10k
   ];
 
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    enableCompletion = true;
-    shellAliases = {
-      gs = "git status";
-      ls = "exa -TlL 1 --color-scale --icons --sort=type --no-time";
-      la = "exa -aTlL1 --color-scale --icons --sort=type --no-time";
-      sudo = "doas";
-      rm = "rm -i";
-      nix-shell = "nix-shell --command zsh";
-      develop = "nix develop --command zsh";
-      cd = "z";
-      cdi = "zi";
-      cat = "bat";
-    };
-
-    oh-my-zsh = {
+  programs = {
+    zsh = {
       enable = true;
-      plugins = [
-        "git"
-        "dircycle colored-man-pages"
-      ];
-    };
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      shellAliases = {
+        gs = "git status";
+        ls = "exa -TlL 1 --color-scale --icons --sort=type --no-time";
+        la = "exa -aTlL1 --color-scale --icons --sort=type --no-time";
+        sudo = "doas";
+        rm = "rm -i";
+        nix-shell = "nix-shell --command zsh";
+        develop = "nix develop --command zsh";
+        cd = "z";
+        cdi = "zi";
+        cat = "bat";
+      };
 
-    # NOTE: noxide evaluation
-    initExtra = ''
-      eval "$(zoxide init zsh)"
-    '';
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "dircycle colored-man-pages"
+        ];
+      };
+
+      # NOTE: noxide evaluation
+      initExtra = ''
+        eval "$(zoxide init zsh)"
+      '';
+    };
 
     oh-my-posh = {
       enable = true;
@@ -47,6 +49,7 @@
       useTheme = "robbyrussell"; # fallback value
       enableZshIntegration = true;
     };
+
     direnv = {
       enable = true;
       enableZshIntegration = true;
