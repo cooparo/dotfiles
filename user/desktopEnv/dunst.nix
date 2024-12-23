f6342f5b1ebced2e5314c23ec6a2e60d8f4bfa7b
cooparo <github.ambitious226@slmails.com>
2024-12-23 07:55:11 +0100

    better ricing for dunst

--- user/desktopEnv/dunst.nix
+++ user/desktopEnv/dunst.nix
@@ -3,28 +3,35 @@
   services.dunst = {
     enable = true;
     package = pkgs.dunst;
+
+    iconTheme = {
+      package = pkgs.qogir-icon-theme;
+      name = "Qogir-dark";
+
+    };
+
     settings = {
       global = with config.colorScheme.palette; {
+        font = "DejaVu Sans 12";
         width = 300;
         height = 300;
         offset = "20x50";
-        transparency = 5;
+        transparency = 3;
         notification_limit = 5;
         frame_color = "#${base0E}";
-        background = "#${base02}";
+        background = "#${base00}";
         foreground = "#ffffff";
-        frame_width = 1;
-        corner_radius = 5;
+        frame_width = 3;
+        corner_radius = 3;
         gap_size = 3;
       };
 
       urgency_low = with config.colorScheme.palette; {
-        frame_color = "#${base02}";
+        frame_color = "#${base0B}";
       };
 
       urgency_critical = with config.colorScheme.palette; {
         frame_color = "#${base08}";
-        background = "#${base08}";
       };
     };
   };
