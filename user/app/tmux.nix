94b2fdeeeb208e60bd0e6b206e7c2232695cb232
cooparo <github.ambitious226@slmails.com>
2024-12-30 15:10:19 +0100

    Stylix manages tmux ricing

--- user/app/tmux.nix
+++ user/app/tmux.nix
@@ -63,8 +63,6 @@ in
         set-option -g allow-rename off
       '';
       plugins = with pkgs; [
-        # FIX: use base16 coloscheme
-        # tmuxPlugins.${theme}
         tmuxPlugins.tmux-fzf
       ];
     };
