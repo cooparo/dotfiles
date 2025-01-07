8f12242ec46bda9e842d184760475eed4cd29488
cooparo <github.ambitious226@slmails.com>
2025-01-07 23:18:35 +0100

    removed useless pkgs

--- host/anon/home.nix
+++ host/anon/home.nix
@@ -1,14 +1,7 @@
-{ pkgs, ... }:
-
 ## DESKTOP USER SETTINGS
 {
   imports = [
     ../../user/default.nix
-  ];
-
-  home.packages = with pkgs; [
-    brightnessctl # Manage display brightness
-    powertop
   ];
 
   batsignal.enable = true;
