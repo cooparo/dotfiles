eee0c9a6528213527075375246b055585b6c969f
cooparo <github.ambitious226@slmails.com>
2025-01-12 14:21:08 +0100

    neovim configured telescope

--- user/app/nvim/lua/plugins/telescope.lua
+++ user/app/nvim/lua/plugins/telescope.lua
@@ -4,3 +4,26 @@ vim.keymap.set('n', '<leader><leader>', builtin.git_files, { desc = 'Telescope g
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+local actions = require('telescope.actions')
+require('telescope').setup({
+	defaults = {
+		mappings = {
+			i = { -- Insert mode mappings
+				["<Esc>"] = actions.close, -- Close on Esc in insert mode
+			},
+			n = { -- Normal mode mappings
+				["<Esc>"] = actions.close, -- Close on Esc in normal mode
+			},
+		},
+		extensions = {
+			-- Configure the fzf-native extension
+			fzf = {
+				fuzzy = true,                    -- Enable fuzzy search
+				override_generic_sorter = true,  -- Override the generic sorter
+				override_file_sorter = true,     -- Override the file sorter
+				case_mode = "smart_case",        -- "smart_case" | "ignore_case" | "respect_case"
+			},
+		}
+	},
+})
