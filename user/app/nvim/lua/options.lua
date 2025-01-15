4fb554de22e2c3d36386cd066f95da4f6bffb8c4
cooparo <github.ambitious226@slmails.com>
2025-01-15 16:50:18 +0100

    removed useless semi-colon

--- user/app/nvim/lua/options.lua
+++ user/app/nvim/lua/options.lua
@@ -10,20 +10,20 @@ vim.o.shiftwidth = 2
 
 vim.o.termguicolors = true
 
-vim.o.smartindent = true; 
-vim.o.wrap = false;
-vim.o.breakindent = true;
-vim.o.backup = false;
-vim.o.swapfile = false;
-vim.o.hlsearch = false;
-vim.o.incsearch = true;
-vim.o.termguicolors = true;
-vim.o.scrolloff = 8;
+vim.o.smartindent = true
+vim.o.wrap = false
+vim.o.breakindent = true
+vim.o.backup = false
+vim.o.swapfile = false
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.termguicolors = true
+vim.o.scrolloff = 8
 
-vim.o.updatetime = 300; 
+vim.o.updatetime = 300
 
-vim.o.undofile = true;
-vim.o.cursorline = true; 
+vim.o.undofile = true
+vim.o.cursorline = true
 
-vim.o.encoding = "utf-8";
-vim.o.fileencoding = "utf-8";
+vim.o.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
