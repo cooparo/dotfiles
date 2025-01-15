8b36104160274943f54dcea2360f0c4e4456e9e9
cooparo <github.ambitious226@slmails.com>
2025-01-15 17:06:26 +0100

    neovim added todo

--- user/app/nvim/lua/plugins/telescope.lua
+++ user/app/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,11 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader><leader>', builtin.git_files, { desc = 'Telescope git files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+local keymap = vim.keymap
+
+keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+keymap.set('n', '<leader><leader>', builtin.git_files, { desc = 'Telescope git files' })
+keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+-- TODO: add TODO/FIX search 
 
 local actions = require('telescope.actions')
 require('telescope').setup({
