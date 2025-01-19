718e774d45fbf71ff46b3b143964296e95bf52f7
cooparo <github.ambitious226@slmails.com>
2025-01-19 13:42:32 +0100

    neovim added cool remapping

--- user/app/nvim/lua/remap.lua
+++ user/app/nvim/lua/remap.lua
@@ -1,12 +1,33 @@
-vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>q", "<cmd>wq<CR>")
+local map = vim.keymap
 
-vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>")
+map.set("i", "jk", "<Esc>")
+map.set("n", "<leader>w", "<cmd>w<CR>")
+map.set("n", "<leader>q", "<cmd>wq<CR>")
+
+map.set("n", "<leader>bd", "<cmd>bd<CR>") -- Delete buffer
+
+-- Move visual block
+map.set("v", "J", ":m '>+1<CR>gv=gv")
+map.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Center cursor
+map.set("n", "<C-d>", "<C-d>zz")
+map.set("n", "<C-u>", "<C-u>zz")
+map.set("n", "n", "nzzzv")
+map.set("n", "N", "Nzzzv")
+
+-- Copy the deleted selection into the void register
+map.set("x", "<leader>p", "\"_dP")
+map.set("n", "<leader>d", "\"_d")
+map.set("v", "<leader>d", "\"_d")
+
+-- Copy to global clipboard
+map.set("v", "<leader>y", "\"+y")
+map.set("n", "<leader>y", "\"+y")
 
 -- Lazygit
-vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+map.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
 
 --Bufferline
-vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
-vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
+map.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
+map.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
