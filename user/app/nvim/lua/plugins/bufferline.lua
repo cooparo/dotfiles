dadbd0d9e0a1699521723b3e809b1dff3dfccf4b
cooparo <github.ambitious226@slmails.com>
2025-01-19 15:27:05 +0100

    neovim added pane split remaps and descriptions

--- user/app/nvim/lua/plugins/bufferline.lua
+++ user/app/nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,7 @@
+--Bufferline
+vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
+
 require('bufferline').setup {
 	options = {
 		separator_style = "thick",
