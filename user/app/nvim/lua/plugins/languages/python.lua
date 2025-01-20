6385cb9d126490992400e3eba91559ea6770b11a
cooparo <github.ambitious226@slmails.com>
2025-01-21 00:58:34 +0100

    neovim moved programming langs to dedicated files

---
+++ user/app/nvim/lua/plugins/languages/python.lua
@@ -0,0 +1,7 @@
+local format = require('lsp-format')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lsp-config').pyright.setup {
+	capabilities = capabilities,
+	on_attach = format.on_attach
+}
--- user/app/nvim/lua/plugins/lsp-format.lua
+++
@@ -1,8 +0,0 @@
-local format = require('lsp-format')
-local lsp = require('lspconfig')
-
-format.setup {}
-
-lsp.nixd.setup { on_attach = format.on_attach }
-lsp.lua_ls.setup { on_attach = format.on_attach }
-lsp.pyright.setup { on_attach = format.on_attach }
