87e8e54e4cc1fd66f89c4f3832cc193346645f30
cooparo <github.ambitious226@slmails.com>
2025-01-21 19:22:19 +0100

    neovim removed jk exit visual mode remap

--- user/app/nvim/lua/remap.lua
+++ user/app/nvim/lua/remap.lua
@@ -1,7 +1,6 @@
 local map = vim.keymap
 
 map.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
-map.set("v", "jk", "<Esc>", { desc = "Exit visual mode" })
 map.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
 map.set("n", "<leader>q", "<cmd>wq<CR>", { desc = "Save & quit file" })
 
