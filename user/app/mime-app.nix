1a1bd9645d3fce014d54e6f588e7d6aa001370e5
cooparo <github.ambitious226@slmails.com>
2025-02-06 20:00:20 +0100

    mime-app added default mime application

--- user/app/mime-app.nix
+++ user/app/mime-app.nix
@@ -16,6 +16,24 @@
 
       defaultApplications = {
         "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
+
+        "text/html" = "brave-browser.desktop";
+        "text/md" = "gnome-text-editor.desktop";
+
+        "x-scheme-handler/http" = "brave-browser.desktop";
+        "x-scheme-handler/https" = "brave-browser.desktop";
+        "x-scheme-handler/about" = "brave-browser.desktop";
+        "x-scheme-handler/unknown" = "brave-browser.desktop";
+        "x-scheme-handler/mailto" = "brave-browser.desktop";
+
+        "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+        "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
+
+        "image/jpeg" = "org.gnome.Loupe.desktop";
+        "image/png" = "org.gnome.Loupe.desktop";
+        "image/xcf" = "gimp.desktop";
+
+        "video/mkv" = "vlc.desktop";
       };
     };
   };
