148546af61bcb55fb9eeae71c44e4236be1cd8dd
cooparo <github.ambitious226@slmails.com>
2025-03-11 14:00:29 +0100

    now using pipewire for audio

--- system/hardware/audio.nix
+++ system/hardware/audio.nix
@@ -13,9 +13,27 @@
     security.rtkit.enable = true;
     users.users.${userSettings.username}.extraGroups = [ "audio" ];
 
-    #TODO: add some infos.
-    # Why pulseaudio is disabled?
-    services.pulseaudio.enable = false;
-    services.pulseaudio.support32Bit = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+    };
+
+    services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+      };
+    };
   };
 }
