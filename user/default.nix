6d989754d72df6812f1ece9dcddc1e3f8c8c7ef6
cooparo <github.ambitious226@slmails.com>
2025-03-13 12:04:14 +0100

    pkgs airgeddon override supportHashCracking = true

--- user/default.nix
+++ user/default.nix
@@ -24,76 +24,85 @@
     inherit (userSettings) username;
     homeDirectory = "/home/${userSettings.username}";
 
-    packages = with pkgs; [
-      # Core
-      brave
-      neofetch
-      git
-      zsh
-      alacritty
-      vlc
-      unzip
+    packages =
+      let
+        # Enabling options disabled by default
+        airgeddonFull = pkgs.airgeddon.override {
+          supportHashCracking = true;
+          # supportEvilTwin = true; # FIX: doesn't build
+        };
+      in
+      with pkgs;
+      [
+        # Core
+        brave
+        neofetch
+        git
+        zsh
+        alacritty
+        vlc
+        unzip
 
-      # App
-      telegram-desktop
-      spotify
-      obsidian
-      discord
-      docker
-      qbittorrent
-      libreoffice
-      vscode
-      tmux
-      electrum
-      zathura
-      zoom-us
+        # App
+        telegram-desktop
+        spotify
+        obsidian
+        discord
+        docker
+        qbittorrent
+        libreoffice
+        vscode
+        tmux
+        electrum
+        zathura
+        zoom-us
 
-      # Language
-      python3
-      lua
-      gdb # GNU project debugger
+        # Language
+        python3
+        lua
+        gdb # GNU project debugger
 
-      # Tools
-      htop
-      playerctl # Media player manager (eg. spotify)
-      pulseaudioFull
-      pavucontrol # Manage audio
-      dhcpcd
-      nssmdns
-      openssl
-      zoxide # smarter cd
-      fzf
-      flameshot
-      bat
-      gitflow
-      merge-fmt
+        # Tools
+        htop
+        playerctl # Media player manager (eg. spotify)
+        pulseaudioFull
+        pavucontrol # Manage audio
+        dhcpcd
+        nssmdns
+        openssl
+        zoxide # smarter cd
+        fzf
+        flameshot
+        bat
+        gitflow
+        merge-fmt
 
-      # Desktop env
-      picom # Compositor
-      rofi # Menu
-      rofi-power-menu
-      autotiling # Open window by golden ratio
-      alsa-utils
-      libnotify # Notification
+        # Desktop env
+        picom # Compositor
+        rofi # Menu
+        rofi-power-menu
+        autotiling # Open window by golden ratio
+        alsa-utils
+        libnotify # Notification
 
-      ## For betterlockscreen
-      bc
-      xorg.xdpyinfo
-      xorg.xrandr
-      xorg.xrdb
-      xorg.xset
-      imagemagick
-      i3lock-color
+        ## For betterlockscreen
+        bc
+        xorg.xdpyinfo
+        xorg.xrandr
+        xorg.xrdb
+        xorg.xset
+        imagemagick
+        i3lock-color
 
-      # CyberSec tools
-      nmap
-      burpsuite
-      seclists
-      exiftool
-      file
-      macchanger
-      airgeddon
-    ];
+        # CyberSec tools
+        nmap
+        burpsuite
+        seclists
+        exiftool
+        file
+        macchanger
+        airgeddonFull
+      ];
 
     sessionVariables = with userSettings; {
       EDITOR = editor;
