973f738bcf7bd1da54b02e143f529db6324576ff
cooparo <github.ambitious226@slmails.com>
2025-03-27 16:27:50 +0100

    injecting colorscheme as env variables for nvim flake use

--- system/tools/environmentVariables.nix
+++ system/tools/environmentVariables.nix
@@ -4,6 +4,26 @@
   userSettings,
   ...
 }:
+with config.lib.stylix.colors;
+let
+  BASE00 = "#${base00}";
+  BASE01 = "#${base01}";
+  BASE02 = "#${base02}";
+  BASE03 = "#${base03}";
+  BASE04 = "#${base04}";
+  BASE05 = "#${base05}";
+  BASE06 = "#${base06}";
+  BASE07 = "#${base07}";
+  BASE08 = "#${base08}";
+  BASE09 = "#${base09}";
+  BASE0A = "#${base0A}";
+  BASE0B = "#${base0B}";
+  BASE0C = "#${base0C}";
+  BASE0D = "#${base0D}";
+  BASE0E = "#${base0E}";
+  BASE0F = "#${base0F}";
+
+in
 {
   options = {
     environmentVariables.enable = lib.mkEnableOption "Enables environment variables";
@@ -14,6 +34,23 @@
       ETH_INTERFACE = eth-interface;
       WIFI_INTERFACE = wifi-interface;
       INTERFACE = if (wifi-interface == "") then eth-interface else wifi-interface;
+
+      inherit BASE00;
+      inherit BASE01;
+      inherit BASE02;
+      inherit BASE03;
+      inherit BASE04;
+      inherit BASE05;
+      inherit BASE06;
+      inherit BASE07;
+      inherit BASE08;
+      inherit BASE09;
+      inherit BASE0A;
+      inherit BASE0B;
+      inherit BASE0C;
+      inherit BASE0D;
+      inherit BASE0E;
+      inherit BASE0F;
     };
   };
 }
